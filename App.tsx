c71b559c2b03786677cf0e14f049c0d7fe319fe8
Nguyễn Chí Cường <nccuong.developer@gmail.com>
2024-02-06 00:15:02 +0700

    add some screen

--- App.tsx
+++ App.tsx
@@ -1,12 +1,29 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { SplashScreen } from './src/screens';
+import AuthNavigator from './src/navigators/AuthNavigator';
+import { NavigationContainer } from '@react-navigation/native';
 
 const App = () => {
+  const [isShowSplash, setIsShowSpalsh] = useState(true);
+
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+      setIsShowSpalsh(false);
+    }, 1500);
+
+    return () => clearTimeout(timeOut);
+  }, []);
   return (
-    <View>
-      <Text>App test</Text>
-    </View>
-  )
+    isShowSplash ? (
+      <SplashScreen />
+    ) : (
+      <NavigationContainer>
+       <AuthNavigator />
+      </NavigationContainer>
+    )
+  );
+  
 }
 
 export default App
